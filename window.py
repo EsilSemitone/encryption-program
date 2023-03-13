44232bfc04aa2a8be911203e73535fbc8fb31965
SemiTone <esilguitara1@mail.ru>
2023-03-13 16:01:36 +0500

    добавил окно вывода

--- window.py
+++ window.py
@@ -5,18 +5,16 @@ from tkinter import ttk
 class MainWindow:
     HEIGHT = 500
     WIDTH = 1000
+    root = tk.Tk()
+    POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
+    POS_Y = root.winfo_screenheight() // 2 - HEIGHT // 2
+    root.geometry(f'{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}')
+    icon = tk.PhotoImage(file='images//icon.png')
+    root.iconphoto(False, icon)
+    root.resizable(False, False)
 
-    def __init__(self, title: str ='Шифровальщик'):
-
-        self.root = tk.Tk()
-
+    def __init__(self, title: str = 'Шифровальщик'):
         self.root.title(title)
-        self.POS_X = self.root.winfo_screenwidth() // 2 - self.WIDTH // 2
-        self.POS_Y = self.root.winfo_screenheight() // 2 - self.HEIGHT // 2
-        self.root.geometry(f'{self.WIDTH}x{self.HEIGHT}+{self.POS_X}+{self.POS_Y}')
-        self.icon = tk.PhotoImage(file='images//icon.png')
-        self.root.iconphoto(False, self.icon)
-        self.root.resizable(False, False)
 
         self.app = MainWindowFrame(self.root)
 
@@ -29,5 +27,15 @@ class MainWindowFrame(tk.Frame):
         self.pack()
 
         ttk.Label(text='Поле для ввода').pack(anchor=tk.SW, padx=5, pady=5)
-        self.try_text = tk.Text(height=10, width=50, wrap=tk.WORD).pack(anchor=tk.SW, fill=None)
-        #self.scroll_try = tk.Scrollbar()
+        self.try_text = tk.Text(height=10, width=50, wrap=tk.WORD)
+        self.try_text.pack(anchor=tk.SW, fill=None)
+        self.scroll_try = tk.Scrollbar(width=23, command=self.try_text.yview)
+        self.try_text["yscrollcommand"] = self.scroll_try.set
+        self.scroll_try.place(x=481, y=36, height=203)
+
+        ttk.Label(text='Поле вывода').pack(anchor=tk.SW, padx=5, pady=5)
+        self.try_text = tk.Text(height=10, width=50, wrap=tk.WORD)
+        self.try_text.pack(anchor=tk.SW, fill=None)
+        self.scroll_try = tk.Scrollbar(width=23, command=self.try_text.yview)
+        self.try_text["yscrollcommand"] = self.scroll_try.set
+        self.scroll_try.place(x=481, y=274, height=203)
